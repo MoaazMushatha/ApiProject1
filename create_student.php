caa753cbbffbf319a748c84d96a16bc4dd871431
MoaazMushatha <130844400+MoaazMushatha@users.noreply.github.com>
2023-05-30 16:05:43 +0300

    Update create_student.php

--- create_student.php
+++ create_student.php
@@ -17,23 +17,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 
-
-// include("config.php");
-
-// $id = $_POST['id'];
-// $name = $_POST['name'];
-// $age = $_POST['age'];
-// $email = $_POST['email'];
-// $mobile_num = $_POST['mobile_num'];
-
-// $sql = "INSERT INTO student (id,name,age,email,mobile_num)VALUES ('$id','$name','$age','$email','$mobile_num')";
-
-// $inserted = true;
-// if ($inserted) {
-//     $response = ['message' => 'add successfuly student'];
-// } else {
-//     $response = ['message' => 'failed to create student'];
-// }
-
-// echo json_encode($response)
 ?>
