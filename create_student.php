7f79790e09cd2e83f3b1ec9b9127ad9807fbf1d9
MoaazMushatha <130844400+MoaazMushatha@users.noreply.github.com>
2023-05-30 15:50:14 +0300

    Add files via upload

---
+++ create_student.php
@@ -0,0 +1,39 @@
+<?php
+include('db_helper.php');
+// include('conn.php');
+header("Content-Type:application/json");
+
+$db_helper = new DbHelper();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $mobile_num = $_POST['mobile_num'];
+
+    $resuilt = $db_helper->insertNewStudent($id, $name, $age, $email, $mobile_num);
+
+
+}
+
+
+
+// include("config.php");
+
+// $id = $_POST['id'];
+// $name = $_POST['name'];
+// $age = $_POST['age'];
+// $email = $_POST['email'];
+// $mobile_num = $_POST['mobile_num'];
+
+// $sql = "INSERT INTO student (id,name,age,email,mobile_num)VALUES ('$id','$name','$age','$email','$mobile_num')";
+
+// $inserted = true;
+// if ($inserted) {
+//     $response = ['message' => 'add successfuly student'];
+// } else {
+//     $response = ['message' => 'failed to create student'];
+// }
+
+// echo json_encode($response)
+?>
---
+++ get_students.php
@@ -0,0 +1,43 @@
+<?php
+include('db_helper.php');
+
+
+//get student 
+header("Content-Type:application/json");
+
+try {
+
+    $db_helper = new DbHelper();
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $db_helper->getAllStudents();
+    } else {
+        throw Exception("No Data Found");
+    }
+
+} catch (Exception $exception) {
+    $db_helper->createResponse(false, 0, array("error" => $exception->getMessage()));
+}
+// $conn = mysqli_connect("localhost", "root", "", "stdmang");
+
+// $query = "SELECT * FROM students";
+
+// $result = mysqli_query($conn, $query);
+
+// $students = [];
+// $count = mysqli_num_rows($result);
+// if ($count > 0) {
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         array_push($students, $row);
+//     }
+//     $db_helper = new DbHelper();
+
+//     $db_helper->createResponse(true, $count, $students);
+
+
+// include 'db_helper.php';
+// header("Content-Type:application/json");
+
+
+
+// echo json_encode($students);
+?>
