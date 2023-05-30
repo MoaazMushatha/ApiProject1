2544547c540b7ba7ed601e3d1121f5a0d5d8e1d9
MoaazMushatha <130844400+MoaazMushatha@users.noreply.github.com>
2023-05-30 16:06:17 +0300

    Update get_students.php

--- get_students.php
+++ get_students.php
@@ -17,27 +17,5 @@ try {
 } catch (Exception $exception) {
     $db_helper->createResponse(false, 0, array("error" => $exception->getMessage()));
 }
-// $conn = mysqli_connect("localhost", "root", "", "stdmang");
-
-// $query = "SELECT * FROM students";
-
-// $result = mysqli_query($conn, $query);
-
-// $students = [];
-// $count = mysqli_num_rows($result);
-// if ($count > 0) {
-//     while ($row = mysqli_fetch_assoc($result)) {
-//         array_push($students, $row);
-//     }
-//     $db_helper = new DbHelper();
-
-//     $db_helper->createResponse(true, $count, $students);
-
-
-// include 'db_helper.php';
-// header("Content-Type:application/json");
-
-
-
-// echo json_encode($students);
+ 
 ?>
